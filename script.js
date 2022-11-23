72b47528556ea4775de25ccded0b082a04896920
Githubtgawlik <tgawlik@interia.pl>
2022-11-23 11:24:28 +0100

    fix: add new files and modyfied

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ for (const bottom of bottoms) {
 ul.append(fragment);
 
 const icon2 = document.createElement("img");
-icon2.setAttribute("src", "../icons/basket.png");
+icon2.setAttribute("src", "./icons/basket.png");
 icon2.setAttribute("id", "icon2");
 icon2.setAttribute("width", "50");
 icon2.setAttribute("height", "50");
@@ -55,46 +55,46 @@ fetch("./books.json")
   });
 
 function appendData(data) {
-  const mainContainer = document.getElementById("section");
-  for (const i = 0; i < data.length; i++) {
-    const div = document.createElement("div");
-    const image_link_div = document.createElement("div");
-    const image_link = document.createElement("img");
-    const image_info = document.createElement("div");
-    const image_info_content = document.createElement("p");
-    const image_info_button = document.createElement("input");
-    const bottom_div = document.createElement("div");
-    const title = document.createElement("h2");
-    const author_p = document.createElement("p");
-    const price_cart_div = document.createElement("div");
-    const price = document.createElement("p");
-    const cart = document.createElement("input");
-    const about_info = document.createElement("input");
+  let mainContainer = document.getElementById("section");
+  for (let i = 0; i < data.length; i++) {
+    let div = document.createElement("div");
+    let book_card_img = document.createElement("div");
+    let book_image_link = document.createElement("img");
+    let book_info = document.createElement("div");
+    let book_info_content = document.createElement("p");
+    let book_info_button = document.createElement("input");
+    let book_description = document.createElement("div");
+    let title = document.createElement("h2");
+    let author_p = document.createElement("p");
+    let price_cart_div = document.createElement("div");
+    let price = document.createElement("p");
+    let cart = document.createElement("input");
+    let about_info = document.createElement("input");
 
-    div.appendChild(image_link_div);
-    image_link_div.appendChild(image_link);
-    image_link.after(image_info);
-    image_info.appendChild(image_info_content);
-    image_link_div.after(bottom_div);
-    image_info_content.after(image_info_button);
-    bottom_div.appendChild(author_p);
+    div.appendChild(book_card_img);
+    book_card_img.appendChild(book_image_link);
+    book_image_link.after(book_info);
+    book_info.appendChild(book_info_content);
+    book_card_img.after(book_description);
+    book_info_content.after(book_info_button);
+    book_description.appendChild(author_p);
     author_p.after(title);
     title.after(price_cart_div);
     price_cart_div.appendChild(price);
     price.after(about_info);
     about_info.after(cart);
 
-    image_link.src = data[i].imageLink;
+    book_image_link.src = data[i].imageLink;
     title.innerHTML = data[i].title;
     author_p.innerHTML = data[i].author;
     price.innerHTML = data[i].price + ` $`;
-    image_info_content.innerHTML = data[i].description;
+    book_info_content.innerHTML = data[i].description;
 
-    div.id = "card_div_id_inner"; // section
-    image_link_div.id = "image_link_div";
-    image_link.id = "image_id";
-    image_info.id = "image_info";
-    bottom_div.id = "bottom_div";
+    div.id = "book_card";
+    book_card_img.id = "book_card_img";
+    book_image_link.id = "book_image";
+    book_info.id = "book_info";
+    book_description.id = "book_description";
     title.id = "title_id";
     author_p.id = "author_id";
     price_cart_div.id = "price_cat_div";
@@ -105,10 +105,14 @@ function appendData(data) {
     cart.id = "cart";
     cart.type = "submit";
     cart.value = "Add to Cart";
-    image_info_button.id = "image_info_button";
-    image_info_button.type = "submit";
-    image_info_button.value = "Close";
+    book_info_button.id = "book_info_button";
+    book_info_button.type = "submit";
+    book_info_button.value = "Close";
 
     mainContainer.appendChild(div);
   }
 }
+
+const footer = document.createElement("footer");
+footer.setAttribute("id", "footer");
+body.append(footer);
